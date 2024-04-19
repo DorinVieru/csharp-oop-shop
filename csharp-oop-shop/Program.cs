15d68e348131cfe17aa8d589ed2a361b58f3aa74
DorinVieru <dorino.vieru@gmail.com>
2024-04-19 15:14:38 +0200

    UP: write characters of product

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace csharp_oop_shop
 {
+    
+    public class Prodotto
+    {
+        private int _codice;
+        public int Codice { get { return _codice; } }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public double IVA { get; set; }
+
+        
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
